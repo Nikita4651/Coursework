3d60d49965a346696b221a99cd2fe53a3ac906e3
Nikitos <nikita4651@yandex.ru>
2026-08-07 12:14:50 +0300

    first commit

--- src/test/java/ru/netology/Data/DataHelper.java
+++ src/test/java/ru/netology/Data/DataHelper.java
@@ -17,11 +17,11 @@ public class DataHelper {
     }
 
     public static CardInfo getFirstCardInfo() {
-        return new CardInfo("5559 0000 0000 0001", "92df3f1c-a033-48e6-8390-206f6b1f56c0");
+        return new CardInfo("1111 2222 3333 4444");
     }
 
     public static CardInfo getSecondCardInfo() {
-        return new CardInfo("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
+        return new CardInfo("5555 6666 7777 8888");
     }
 
     public static int generateValidAmount(int balance) {
